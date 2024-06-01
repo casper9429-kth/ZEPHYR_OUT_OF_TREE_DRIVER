609fad0b3c457fa008f10b1e0db8ecabf29cde61
Casper <cas.aug.sav@gmail.com>
2024-06-01 22:28:46 +0200

    cleaned .h file

--- drivers/sensor/pyd1598/pyd1598.h
+++ drivers/sensor/pyd1598/pyd1598.h
@@ -9,36 +9,10 @@ extern "C" {
 #include <zephyr/drivers/sensor.h>
 
 
-/**
- * @brief Zero the HX711.
- *
- * @param dev Pointer to the hx711 device structure
- * @param readings Number of readings to get average offset.
- *        5~10 readings should be enough, although more are allowed.
- * @retval The offset value
- *
- */
-int excelitas_pyd1598_tare(const struct device *dev, uint8_t readings);
-
-/**
- * @brief Callibrate the HX711.
- *
- * Given a target value of a known weight the slope gets calculated.
- * This is actually unit agnostic.
- * If the target weight is given in grams, lb, Kg or any other weight unit,
- * the slope will be calculated accordingly.
- *
- * @param dev Pointer to the hx711 device structure
- * @param readings Number of readings to take for calibration.
- *        5~10 readings should be enough, although more are allowed.
- * @retval The slope value
- *
- */
-struct sensor_value excelitas_pyd1598_calibrate(const struct device *dev,
-					 uint8_t readings);
+// Fill in with functions when implemented
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* ZEPHYR_INCLUDE_DRIVERS_SENSOR_HX711_H_ */
+#endif /* ZEPHYR_INCLUDE_DRIVERS_SENSOR_PYD1598_H_ */
